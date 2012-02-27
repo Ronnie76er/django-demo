8877160630c3d4482bda554a2620fd16315d70ff
Ron Alleva <ron.alleva@gmail.com>
2012-02-26 21:39:37 -0500

    Enable the admin functions

--- mysite/testapp/models.py
+++ mysite/testapp/models.py
@@ -4,9 +4,12 @@ class TestUser(models.Model):
 	username = models.CharField(max_length = 200)
 	create_date = models.DateTimeField('Date Created')
 
+	def __unicode__(self):
+		return self.username
+
 class UserInfo(models.Model):
 	user = models.ForeignKey(TestUser)
 	data_field1 = models.CharField(max_length=300)
-	
+
 
 # Create your models here.
